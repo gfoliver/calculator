61ff0fab838a097bde38660e7a2bd509e225aea7
Guilherme Fleck Oliveira <guilhermefleckoliveira@gmail.com>
2020-04-18 15:09:27 -0300

    App version 0.1.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './app.scss'
 
 function App() {
+  const [displayContent, setDisplayContent] = useState('')
+  const numbers = [9,8,7,6,5,4,3,2,1]
+
+  function equals() {
+    setDisplayContent(String(eval(displayContent)))
+  }
+
+  function changeSignal() {
+    if (displayContent[0] == '-')
+      setDisplayContent(displayContent.slice(1))
+    else
+      setDisplayContent('-' + displayContent)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="display">
+        <div className="content">{displayContent}</div>
+      </div>
+      <div className="buttons">
+        <div className="top-controls">
+          <button onClick={changeSignal}>+/-</button>
+          <button onClick={() => setDisplayContent('')}>C</button>
+          <button onClick={() => setDisplayContent(displayContent.slice(0, -1))}><i className="fas fa-backspace"></i></button>
+        </div>
+        <div className="right-controls">
+          <button onClick={() => setDisplayContent(displayContent + '/')}><i className="fas fa-divide"></i></button>
+          <button onClick={() => setDisplayContent(displayContent + '*')}><i className="fas fa-times"></i></button>
+          <button onClick={() => setDisplayContent(displayContent + '-')}><i className="fas fa-minus"></i></button>
+          <button onClick={() => setDisplayContent(displayContent + '+')}><i className="fas fa-plus"></i></button>
+          <button onClick={equals}><i className="fas fa-equals"></i></button>
+        </div>
+        <div className="numbers">
+          {numbers.map(i => (
+            <button key={i} onClick={() => setDisplayContent(displayContent + i)}>{i}</button>
+          ))}
+          <button className="zero" onClick={() => setDisplayContent(displayContent + 0)}>0</button>
+          <button onClick={() => setDisplayContent(displayContent + '.')}>.</button>
+        </div>
+      </div>
     </div>
   );
 }
